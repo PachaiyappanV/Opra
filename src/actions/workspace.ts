39a7dcc663feb1009ade107fd117a4438f67994c
PachaiyappanV <pachaiyappan394@gmail.com>
2025-01-26 11:40:08 +0530

    feat(video): add server action to fetch workspace videos
    - Implemented `getWorkspaceVideos` server action to retrieve videos associated with a specific workspace.
    - Includes video details such as title, creation date, source, processing status, and related folder and user information.
    - Ensures results are ordered by creation date in ascending order.
    - Returns a status of 200 with video data, 401 for unauthorized access, and 500 for server errors.

--- src/actions/workspace.ts
+++ src/actions/workspace.ts
@@ -2,7 +2,7 @@
 
 import { client } from "@/lib/prisma";
 import { currentUser } from "@clerk/nextjs/server";
-export const verifyAccessToWorkspace = async (workspaceId: string) => {
+export const verifyAccessToWorkspace = async (workSpaceId: string) => {
   try {
     const user = await currentUser();
     if (!user) {
@@ -11,7 +11,7 @@ export const verifyAccessToWorkspace = async (workspaceId: string) => {
 
     const isUserInWorkspace = await client.workSpace.findUnique({
       where: {
-        id: workspaceId,
+        id: workSpaceId,
         OR: [
           {
             User: {
@@ -57,5 +57,49 @@ export const getWorkspaceFolders = async (workSpaceId: string) => {
     return { status: 404, data: [] };
   } catch (error) {
     return { status: 500, data: [] };
+  }
+};
+
+export const getWorkspaceVideos = async (workSpaceId: string) => {
+  try {
+    const user = await currentUser();
+    if (!user) {
+      return { status: 401 };
+    }
+
+    const videos = await client.video.findMany({
+      where: {
+        workSpaceId,
+      },
+      select: {
+        id: true,
+        title: true,
+        createdAt: true,
+        source: true,
+        processing: true,
+        Folder: {
+          select: {
+            id: true,
+            name: true,
+          },
+        },
+        User: {
+          select: {
+            firstname: true,
+            lastname: true,
+            image: true,
+          },
+        },
+      },
+      orderBy: {
+        createdAt: "asc",
+      },
+    });
+
+    if (videos && videos.length) {
+      return { status: 200, data: { videos } };
+    }
+  } catch (error) {
+    return { status: 500 };
   }
 };
