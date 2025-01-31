2baeab7a3c965b7a19398cf26d9e0deca4d21dc5
PachaiyappanV <pachaiyappan394@gmail.com>
2025-01-31 15:57:26 +0530

    feat(ui): add SidebarItem component for navigation menu
    - Created `SidebarItem` component with support for icons, titles, links, and notifications.
    - Includes dynamic styling for selected and hover states.
    - Uses `cn` utility for conditional class handling.
    - Enhances the sidebar's user experience with a clean and responsive design.

---
+++ src/components/global/sidebar/sidebar-item.tsx
@@ -0,0 +1,47 @@
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+
+type Props = {
+  icon: React.ReactNode;
+  title: string;
+  href: string;
+  selected: boolean;
+  notifications?: number;
+};
+const SidebarItem = ({ icon, title, href, selected, notifications }: Props) => {
+  return (
+    <li className=" cursor-pointer my-[5px]">
+      <Link
+        href={href}
+        className={cn(
+          "flex rounded-lg justify-between items-center group hover:bg-[#1D1D1D]",
+          selected ? "bg-[#1D1D1D]" : ""
+        )}
+      >
+        <div className="flex  items-center  gap-2 transition-all p-[5px] cursor-pointer">
+          {icon}
+          <span
+            className={cn(
+              "font-medium group-hover:text-[#9D9D9D] transition-all truncate w-32",
+              selected ? "text-[#9D9D9D]" : "text-[#545454]"
+            )}
+          >
+            {title}
+          </span>
+        </div>
+        {notifications !== 0 && (
+          <span
+            className={cn(
+              "font-medium  mr-1 transition-all group-hover:bg-[#111111] px-2 rounded-sm text-[#9D9D9D]",
+              selected ? "bg-[#111111]" : "bg-[#1D1D1D]"
+            )}
+          >
+            {notifications}
+          </span>
+        )}
+      </Link>
+    </li>
+  );
+};
+
+export default SidebarItem;
