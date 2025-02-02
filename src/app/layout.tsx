1c199d88d0da63142cb982c3fc47382524f78d34
PachaiyappanV <pachaiyappan394@gmail.com>
2025-02-02 09:37:04 +0530

    feat(root-layout): integrate dark theme for ClerkProvider appearance

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { ThemeProvider } from "next-themes";
 import { ClerkProvider } from "@clerk/nextjs";
 import ReactQueryProvider from "@/react-query";
+import { dark } from "@clerk/themes";
 
 const manrope = Manrope({
   subsets: ["latin"],
@@ -20,7 +21,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        baseTheme: [dark],
+      }}
+    >
       <html lang="en" suppressHydrationWarning>
         <body className={`${manrope.className} bg-[#171717]`}>
           <ThemeProvider
