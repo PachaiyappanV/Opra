920a8400000a9fc0c07f3fadb407b40a8c674ad8
PachaiyappanV <pachaiyappan394@gmail.com>
2025-02-13 13:22:16 +0530

    feat(component): add CardMenu  for video relocation
    - Implement modal with `ChangeVideoLocation` form.
    - Add `Move` icon as trigger.

---
+++ src/components/global/videos/video-card-menu.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import Modal from "../modal";
+import { Move } from "lucide-react";
+import ChangeVideoLocation from "@/components/forms/change-video-location";
+
+type Props = {
+  videoId: string;
+  currentWorkspace?: string;
+  currentFolder?: string;
+  currentFolderName?: string;
+};
+
+const CardMenu = ({
+  videoId,
+  currentFolder,
+  currentFolderName,
+  currentWorkspace,
+}: Props) => {
+  return (
+    <Modal
+      className="flex items-center cursor-pointer gap-x-2"
+      title="Move to new Workspace/Folder"
+      description="This action cannot be undone."
+      trigger={<Move size={20} className="text-[#b4b3b3]" />}
+    >
+      <ChangeVideoLocation
+        currentFolder={currentFolder}
+        currentWorkSpace={currentWorkspace}
+        videoId={videoId}
+        currentFolderName={currentFolderName}
+      />
+    </Modal>
+  );
+};
+
+export default CardMenu;
