fc1abbaf9278f750a2017befbea3e9cf4c08f1e6
PachaiyappanV <pachaiyappan394@gmail.com>
2025-02-20 21:01:19 +0530

    feat(api): add auth route for retrieving or creating user profile for recorder
    - Implemented a GET API route to fetch user profile data based on Clerk ID.
    - Integrated Prisma client to query user details, including `studio` and `subscription` info.
    - Added fallback logic to create a new user in the database if not found.
    - Utilized Clerk API to retrieve user details when creating a new user profile.
    - Ensured workspace and subscription entries are initialized for new users.
    - Added error handling for better debugging and response management.

---
+++ src/app/api/auth/[id]/route.ts
@@ -0,0 +1,67 @@
+import { client } from "@/lib/prisma";
+import { clerkClient } from "@clerk/nextjs/server";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const { id } = await params;
+  console.log("Enpoint hit ✅");
+
+  try {
+    const userProfile = await client.user.findUnique({
+      where: {
+        clerkid: id,
+      },
+      include: {
+        studio: true,
+        subscription: {
+          select: {
+            plan: true,
+          },
+        },
+      },
+    });
+    if (userProfile)
+      return NextResponse.json({ status: 200, user: userProfile });
+
+    const clerkUserInstance = await clerkClient();
+    const clerkUser = await clerkUserInstance.users.getUser(id);
+
+    const createUser = await client.user.create({
+      data: {
+        clerkid: id,
+        email: clerkUser.emailAddresses[0].emailAddress,
+        firstname: clerkUser.firstName,
+        lastname: clerkUser.lastName,
+        studio: {
+          create: {},
+        },
+        workspace: {
+          create: {
+            name: `${clerkUser.firstName}'s Workspace`,
+            type: "PERSONAL",
+          },
+        },
+        subscription: {
+          create: {},
+        },
+      },
+      include: {
+        subscription: {
+          select: {
+            plan: true,
+          },
+        },
+      },
+    });
+
+    if (createUser) return NextResponse.json({ status: 201, user: createUser });
+
+    return NextResponse.json({ status: 400 });
+  } catch (error) {
+    console.log("ERROR", error);
+    return NextResponse.json({ status: 500 });
+  }
+}
