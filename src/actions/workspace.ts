ab7dcc0a9d1b88bb6e524534000668391ccb9f3e
PachaiyappanV <pachaiyappan394@gmail.com>
2025-02-28 13:43:50 +0530

    feat(action): add server action  to edit video information

--- src/actions/workspace.ts
+++ src/actions/workspace.ts
@@ -334,3 +334,23 @@ export const getPreviewVideo = async (videoId: string) => {
     return { status: 500 };
   }
 };
+
+export const editVideoInfo = async (
+  videoId: string,
+  title: string,
+  description: string
+) => {
+  try {
+    const video = await client.video.update({
+      where: { id: videoId },
+      data: {
+        title,
+        description,
+      },
+    });
+    if (video) return { status: 200, message: "Video successfully updated" };
+    return { status: 404, message: "Video not found" };
+  } catch (error) {
+    return { status: 400 };
+  }
+};
