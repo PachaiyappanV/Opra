c981b93f98a837a4aa8328bea3c006c3c1816ad4
PachaiyappanV <pachaiyappan394@gmail.com>
2025-03-05 15:29:00 +0530

    feat(component): add UserId component for copying user ID to clipboard

---
+++ src/components/global/user-id/index.tsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import { Check, Copy } from "lucide-react";
+import { useUser } from "@clerk/nextjs";
+import { Button } from "@/components/ui/button";
+
+const UserId = () => {
+  const { user } = useUser();
+  const [copied, setCopied] = useState(false);
+
+  const handleCopy = async () => {
+    try {
+      await navigator.clipboard.writeText(user?.id as string);
+      setCopied(true);
+      setTimeout(() => setCopied(false), 2000); // Reset after 2 seconds
+    } catch (err) {
+      console.error("Failed to copy user ID", err);
+    }
+  };
+
+  return (
+    <Button
+      variant="outline"
+      className="bg-transparent text-[#707070] flex items-center gap-2"
+    >
+      <span className="flex items-center gap-2">user_id</span>
+      <span onClick={handleCopy}>
+        {copied ? <Check size={15} /> : <Copy size={15} />}
+      </span>
+    </Button>
+  );
+};
+
+export default UserId;
